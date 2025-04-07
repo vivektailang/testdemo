61ce70c45e7e8245521154fa7fbb971669fd76db
vivek_tailang@hotmail.com <vivek_tailang@hotmail.com>
2025-04-07 14:01:49 +0530

    updated modules and about us page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { MissionComponent } from './mission/mission.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactComponent } from './contact/contact.component';
+import { JourneyComponent } from './journey/journey.component';
+import { MessagesComponent } from './messages/messages.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'about', component: AboutComponent },
+  { path: 'mission', component: MissionComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'journey', component: JourneyComponent },
+  { path: 'messages', component: MessagesComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' }
+];
